0f5b94de41ab25ad2a786501a29376cef2f0e20f
Maxi Jofré <maxijofre.c@gmail.com>
2021-11-25 21:10:45 -0300

    env vars

--- index.ts
+++ index.ts
@@ -10,14 +10,34 @@
 import * as express from "express";
 const app = express();
 
+
+const dev = process.env.NODE_ENV == "development";
+
+console.log(dev);
+
 //variable de ambiente
 const port = process.env.PORT || 3000;
+
+// para declarar una variable de ambiente en la terminal
+// ej : export USER_FULLNAME=maxi
+// para hacer como un console desde la consola echo $USER_FULLNAME
+// console.log(process.env.USER_FULLNAME);
+
+
+// generamos un endpoint en mi api llamado env
+// devuelve un json
+app.get("/env", (req, res) => {
+  res.json({
+    environment: process.env.NODE_ENV,
+  });
+});
 
 app.get("/hola", (req, res) => {
   res.json({
     message: "Hola soy el servidor, heroku",
   });
 });
+
 app.listen(port, () => {
   console.log(`hola soy express y estoy corriendo en el puerto: ${port}`);
 });
